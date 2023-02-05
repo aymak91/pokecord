751f19789072a1860c8f21648f2bff5fcac34493
aymak91 <amakyu@gmail.com>
2023-02-04 22:57:47 -0800

    redux store setup

--- src/components/Channel.js
+++ src/components/Channel.js
@@ -1,14 +1,26 @@
 import { HashtagIcon } from '@heroicons/react/outline'
 import React from 'react'
+import {useDispatch} from 'react-redux';
+import {useNavigate} from 'react-router-dom';
+import { setChannelInfo } from '../features/channelSlice';
 
 function Channel({id, channelName}) {
-    const setChannel= () => {};
-    
+    const dispatch = useDispatch()
+    const navigate = useNavigate();
+
+    const setChannel= () => {
+        dispatch(setChannelInfo({
+            channelId: id,
+            channelName: channelName,
+        }));
+        navigate(`/channels/${id}`);
+    };
+
     return (
-        <div className='font-medium flex items-center cursor-pointer hover:bg-discord_channelHoverBg p-1 rounded-md hover:text-white'>
-            <HashtagIcon className='h-5 mr-2' onClick={setChannel} /> {channelName}
+        <div className='font-medium flex items-center cursor-pointer hover:bg-discord_channelHoverBg p-1 rounded-md hover:text-white' onClick={setChannel}>
+            <HashtagIcon className='h-5 mr-2' /> {channelName}
         </div>
     )
 }
 
-export default Channel
+export default Channel;
